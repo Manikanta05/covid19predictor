10a29dad8c7bcc578c55d28c78f08f22ef29eed0
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2020-08-04 12:50:25 +0530

    Create init.R

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("openxlsx","shiny","shinythemes","dplyr","caTools")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
