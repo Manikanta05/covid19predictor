110573239cd7a96aa5a43f5577dc4149bee7eca1
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2020-08-09 15:29:52 +0530

    Update run.r

--- run.r
+++ run.r
@@ -3,5 +3,14 @@
 # Combining both server and ui into a Shinyapp
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
+library(shiny)
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
 # Hurray ! 
 ############################################################################
