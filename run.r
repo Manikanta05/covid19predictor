81a6decaab43b0817cb20e99d47ced4f9fed7fdb
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2020-08-09 14:55:11 +0530

    Create run.r

---
+++ run.r
@@ -0,0 +1,7 @@
+###########################################################################
+# The last but not the least part 
+# Combining both server and ui into a Shinyapp
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
+# Hurray ! 
+############################################################################
